1e07f649f0c147092aaa60ac3fc9944cfd12419b
viktor073 <viktor_karyakin@mail.ru>
2020-07-12 11:51:32 +0400

    делаем БАГ

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard NEW NEW</div>
+                <div class="card-header">Dashboard NEW NEW
 
                 <div class="card-body">
                     @if (session('status'))
